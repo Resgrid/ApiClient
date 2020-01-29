61add0e7b90adf424d6c893ee3040ff04ce44dc7
Shawn Jackson <shawn@resgrid.com>
2020-01-29 08:22:53 -0800

    Added call priorities enum and location helpers.

---
+++ Resgrid.ApiClient/Common/CallPriority.cs
@@ -0,0 +1,13 @@
+﻿namespace Resgrid.ApiClient.Common
+{
+	/// <summary>
+	/// Default call (dispatch) priorities for the Resgrid system
+	/// </summary>
+	public enum CallPriority
+	{
+		Low = 0,
+		Medium = 1,
+		High = 2,
+		Emergency = 3
+	}
+}
